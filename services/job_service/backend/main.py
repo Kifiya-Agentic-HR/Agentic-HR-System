13c44be07622311906783bc9061cead31b55cc95
yohannesalex <yohannesalemayehu624@gmail.com>
2025-03-03 09:45:10 +0300

    added the all job_service files

---
+++ services/job_service/backend/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from app.routes.jobs import router as job_router
+from app.routes.applications import router as application_router
+
+app = FastAPI(title="Jobs API")
+
+app.include_router(job_router, prefix="/jobs", tags=["Jobs"])
+app.include_router(application_router, prefix="/applications", tags=["Applications"])
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
