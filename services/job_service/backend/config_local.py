5ca769892126f126baf19cd8a740b137f66f8e2e
yohannesalex <yohannesalemayehu624@gmail.com>
2025-03-04 10:27:43 +0300

    update the config file

--- services/job_service/backend/config_local.py
+++ services/job_service/backend/config_local.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     RABBITMQ_URL = os.getenv("RABBITMQ_URL", "amqp://guest:guest@rabbitmq:5672/")
-    MONGODB_URL = os.getenv("MONGODB_URI", "mongodb://mongodb:27017/resume_db")
+    MONGODB_URL = os.getenv("MONGODB_URI", "mongodb://mongodb:27027/resume_db")
     QUEUE_NAME = os.getenv("QUEUE_NAME", "application_queue")
     UPLOAD_DIR = os.getenv("UPLOAD_DIR", "/shared_volume")
 
