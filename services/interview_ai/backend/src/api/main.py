3012f197a5fd4203bc1f39eff64bd3efd8aff194
Abdulmunim <66489217+abdulmunimjemal@users.noreply.github.com>
2025-03-06 09:46:05 +0300

    [ENH] Include the path in the main

--- services/interview_ai/backend/src/api/main.py
+++ services/interview_ai/backend/src/api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.api.routes.interview import router as interview_router
+from src.api.routes.ingestion import router as ingestion_router
 from src.api.db.dependencies import setup_dependencies
 
 def create_app():
@@ -16,8 +17,10 @@ def create_app():
     
     setup_dependencies(app)
     # set base /api/v1
-    app.include_router(interview_router, prefix="/api/v1")
+    app.include_router(interview_router, tags=["agentic", "HR", "Interviewer"] prefix="/api/v1")
     app.include_router(interview_router)
+    app.include_router(ingestion_router, tags=["agentic", "RAG"], prefix="/api/v1")
+    app.include_router(ingestion_router,  tags=["agentic", "RAG"],)
     
     return app
 
