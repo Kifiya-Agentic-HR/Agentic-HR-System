fbaa749489610b8b555f8fac5b68d7c9488c7dc7
Samrwitt <samrawitkta@gmail.com>
2025-03-06 08:47:22 +0300

    addition of cors

--- services/job_service/backend/main.py
+++ services/job_service/backend/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from app.routes.jobs import router as job_router
 from app.routes.applications import router as application_router
+from fastapi.middleware.cors import CORSMiddleware  
 
 app = FastAPI(title="Jobs API")
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(job_router, prefix="/jobs", tags=["Jobs"])
 app.include_router(application_router, prefix="/applications", tags=["Applications"])
