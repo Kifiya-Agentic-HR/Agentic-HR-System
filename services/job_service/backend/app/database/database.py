56ed216921f369ccfebd676fe2b6084dd2f643dd
yohannesalex <yohannesalemayehu624@gmail.com>
2025-03-06 10:39:41 +0300

    fix some time issume when connnecting to the database

--- services/job_service/backend/app/database/database.py
+++ services/job_service/backend/app/database/database.py
@@ -9,24 +9,11 @@ logger = logging.getLogger(__name__)
 class Database:
     def __init__(self):
         mongo_uri = Config.MONGODB_URL
-        if not mongo_uri:
-            raise ValueError("Database String Initialization Failed.")
-        retry_attempts = 5
-        attempt = 0
-
-        while attempt < retry_attempts:
-            try:
-                self.client = MongoClient(mongo_uri, serverSelectionTimeoutMS=5000)
+        
+        self.client = MongoClient(mongo_uri, serverSelectionTimeoutMS=5000)
                 # Force a connection to check for errors
-                self.client.server_info()
-                logger.info("The database connected successfully.")
-                break
-            except errors.ServerSelectionTimeoutError as err:
-                attempt += 1
-                logger.error(f"Attempt {attempt} of {retry_attempts} failed: {err}")
-                if attempt == retry_attempts:
-                    raise Exception(f"Could not connect to MongoDB after {retry_attempts} attempts: {err}")
-                time.sleep(2)  # wait 2 seconds before retrying
+        self.client.server_info()
+        logger.info("The database connected successfully.")
 
         self.db = self.client.hr_db
         
