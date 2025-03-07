5b8d25cd6ca25d4489d5a471a5357c7e173dc321
yohannesalex <yohannesalemayehu624@gmail.com>
2025-03-07 11:58:35 +0300

    update the data base to handle another request

--- services/job_service/backend/app/database/database.py
+++ services/job_service/backend/app/database/database.py
@@ -1,5 +1,7 @@
 from pymongo import MongoClient, errors
+import os
 import logging
+import time
 from config_local import Config
 
 logger = logging.getLogger(__name__)
@@ -7,12 +9,15 @@ logger = logging.getLogger(__name__)
 class Database:
     def __init__(self):
         mongo_uri = Config.MONGODB_URL
-        
-        self.client = MongoClient(mongo_uri, serverSelectionTimeoutMS=5000)
-                # Force a connection to check for errors
-        self.client.server_info()
-        logger.info("The database connected successfully.")
+        if not mongo_uri:
+            raise ValueError("Failed!")
+        try:
 
+            self.client = MongoClient(mongo_uri)
+            logger.info('database connected successfully')
+        except Exception as e:
+            logger.error("faild to connect to the database")
+        
         self.db = self.client.hr_db
         
         # Pre-load collections if desired
@@ -20,6 +25,8 @@ class Database:
         self.applications_collection = self.db["applications"]
         self.candidates_collection = self.db["candidates"]
         self.interviews_collection = self.db["interviews"]
+        self.screening_results_collection = self.db["screening_results"]
+
     def get_collection(self, name: str):
         return self.db[name]
 
