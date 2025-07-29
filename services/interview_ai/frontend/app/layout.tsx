d42ecd598d85a7fddf19afcc274dc316a2312013
ruthalemfanta <ruthalemfanta@gmail.com>
2025-07-29 15:05:46 +0300

    toast options removed

--- services/interview_ai/frontend/app/layout.tsx
+++ services/interview_ai/frontend/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Toaster  toastOptions={{className:"text-black"}}/>
+        <Toaster  />
         </body>
     </html>
   );
